8f5526675c2df39a1d2975db511dd3994f560a3e
Grosbin <grosbin.rivera@unah.hn>
2023-02-27 23:32:56 -0600

    static files on the server 4

--- portafolioDevDir/settings/base.py
+++ portafolioDevDir/settings/base.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_HOST = 'smtp.googlemail.com'
 
